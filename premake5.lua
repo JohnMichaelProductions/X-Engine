cf41476486b5ca75edc9a6e49f0324640660ddd3
JohnMBurnside <JohnMBurnside21@gmail.com>
2020-02-27 19:25:31 -0800

    Added Premake5 file, working

---
+++ premake5.lua
@@ -0,0 +1,50 @@
+workspace "XEngine"
+	architecture "x64"
+	configurations { "Debug", "Release", "Dist" }
+outputDir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+project "XEngine"
+	location "XEngine"
+	kind "SharedLib"
+	language "C++"
+	targetdir ("bin/" .. outputDir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputDir .. "/%{prj.name}")
+	files { "%{prj.name}/src/**.h", "%{prj.name}/src/**.cpp" }
+	includedirs { "%{prj.name}/vendor/spdlog/include" }
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+		defines { "X_PLATFORM_WINDOWS", "X_BUILD_DLL" }
+		postbuildcommands { ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputDir .. "/RainfallCity") }
+	filter "configurations:Debug"
+		defines "X_DEBUG"
+		symbols "On"
+	filter "configurations:Release"
+		defines "X_RELEASE"
+		optimize "On"
+	filter "configurations:Dist"
+		defines "X_DIST"
+		optimize "On"
+project "RainfallCity"
+	location "RainfallCity"
+	kind "ConsoleApp"
+	language "C++"
+	targetdir ("bin/" .. outputDir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputDir .. "/%{prj.name}")
+	files { "%{prj.name}/src/**.h", "%{prj.name}/src/**.cpp" }
+	includedirs { "XEngine/vendor/spdlog/include", "XEngine/src"}
+	links { "XEngine" }
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+		defines { "X_PLATFORM_WINDOWS" }
+	filter "configurations:Debug"
+		defines "X_DEBUG"
+		symbols "On"
+	filter "configurations:Release"
+		defines "X_RELEASE"
+		optimize "On"
+	filter "configurations:Dist"
+		defines "X_DIST"
+		optimize "On"
