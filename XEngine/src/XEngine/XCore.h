8002a087b8cfbf01cb52c12bcdf2fbb7da853ce6
John Michael Burnside <51937396+JohnMBurnside@users.noreply.github.com>
2020-03-07 22:15:18 -0800

    Update XCore.h

--- XEngine/src/XEngine/XCore.h
+++ XEngine/src/XEngine/XCore.h
@@ -9,6 +9,9 @@
 #else
 	#error X-Engine only supports Windows 64-bit
 #endif
+#ifdef X_DEBUG
+	#define X_ENABLE_ASSERTS
+#endif
 #ifdef X_ENABLE_ASSERTS
 	#define X_ASSERT(x, ...) {if(!(x)) {X_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
 	#define X_CORE_ASSERT(x, ...)  {if(!(x)) {X_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
