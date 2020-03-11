0e9e01bebc865967196912b4230e2ec37e908e8b
JohnMBurnside <JohnMBurnside21@gmail.com>
2020-03-10 21:08:25 -0700

    Added Mouse and Key Codes

--- XEngine/src/XEngine.h
+++ XEngine/src/XEngine.h
@@ -2,6 +2,9 @@
 // Basically acts as a namespace for other projects
 #include <stdio.h>
 #include "XEngine/Application.h"
+#include "XEngine/Input.h"
+#include "XEngine/InputCodes/KeyCodes.h"
+#include "XEngine/InputCodes/MouseCodes.h"
 #include "XEngine/Layer.h"
 #include "XEngine/Log.h"
 #include "XEngine/ImGui/ImGuiLayer.h"
