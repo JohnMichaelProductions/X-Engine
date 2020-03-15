98d86936a5e4da92af1f9cc269296721455d84aa
JohnMBurnside <JohnMBurnside21@gmail.com>
2020-03-14 20:53:22 -0700

    Updated premake files in the PremakeFiles dir

--- PremakeFiles/ImGui/premake5.lua
+++ PremakeFiles/ImGui/premake5.lua
@@ -1,12 +1,18 @@
 project "ImGui"
     kind "StaticLib"
     language "C++"
+    cppdialect "C++17"
+    staticruntime "on"
     targetdir ("bin/%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}/%{prj.name}")
     objdir ("bin-int/%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}/%{prj.name}")
 	files { "imconfig.h", "imgui.h", "imgui.cpp", "imgui_draw.cpp", "imgui_internal.h", "imgui_widgets.cpp", "imstb_rectpack.h", "imstb_textedit.h", "imstb_truetype.h", "imgui_demo.cpp" }
 	filter "system:windows"
         systemversion "latest"
-        cppdialect "C++17"
-        staticruntime "On"
-    filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"
+	filter "configurations:Debug"
+		--buildoptions "/MDd"
+		symbols "on"
+		runtime "Debug"
+	filter "configurations:Release"
+		--buildoptions "/MD"
+		optimize "on"
+		runtime "Release"
