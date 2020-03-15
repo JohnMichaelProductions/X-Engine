f6594ed77a5e218e2e0ebe993dd2ea1eeb626546
JohnMBurnside <JohnMBurnside21@gmail.com>
2020-03-14 21:14:42 -0700

    Got rid of comments in premake files

--- PremakeFiles/GLFW/premake5.lua
+++ PremakeFiles/GLFW/premake5.lua
@@ -15,10 +15,8 @@ project "GLFW"
 		files { "src/win32_init.c", "src/win32_joystick.c", "src/win32_monitor.c", "src/win32_time.c", "src/win32_thread.c", "src/win32_window.c", "src/wgl_context.c", "src/egl_context.c", "src/osmesa_context.c" }
 		defines { "_GLFW_WIN32", "_CRT_SECURE_NO_WARNINGS" }
 	filter "configurations:Debug"
-		--buildoptions "/MDd"
 		symbols "on"
 		runtime "Debug"
 	filter "configurations:Release"
-		--buildoptions "/MD"
 		optimize "on"
 		runtime "Release"
