643781fc2084ff6e9c485c12b81776b7aa62d2ad
JohnMBurnside <JohnMBurnside21@gmail.com>
2020-03-19 19:13:18 -0700

    Added premake5

--- GenerateProject.bat
+++ GenerateProject.bat
@@ -1,4 +1,4 @@
 COPY PremakeFiles\GLFW\premake5.lua XEngine\Dependencies\GLFW\
 COPY PremakeFiles\ImGui\premake5.lua XEngine\Dependencies\ImGui
-CALL Dependencies\bin\premake\premake5.exe vs2019
+CALL Dependencies\premake\premake5.exe vs2019
 PAUSE
