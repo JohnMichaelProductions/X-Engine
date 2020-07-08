24df688b24b8e4288e499eb2fd6b7abc6f93d740
JohnMBurnside <JohnMBurnside21@gmail.com>
2020-07-07 21:52:53 -0700

    Updates to GenerateProject.bat file

--- GenerateProject.bat
+++ GenerateProject.bat
@@ -1,5 +1,7 @@
 COPY PremakeFiles\GLFW\premake5.lua XEngine\vendor\GLFW\
 COPY PremakeFiles\ImGui\premake5.lua XEngine\vendor\ImGui
-RMDIR /Q /S PremakeFiles
+COPY PremakeFiles\XEngine\premake5.lua
 CALL vendor\premake\premake5.exe vs2019
+RMDIR /Q /S PremakeFiles
+DEL premake5.lua
 DEL GenerateProject.bat
